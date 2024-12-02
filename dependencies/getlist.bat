029557ec26d36215be82aff3ae7164e931da0906
Joakim Sandström <joakim.s@webbkontakt.net>
2024-12-02 12:01:58 +0100

    Update getlist.bat
    Remove the dependecie on lfn.exe

--- dependencies/getlist.bat
+++ dependencies/getlist.bat
@@ -5,5 +5,5 @@ cd "%USERPROFILE%\AppData\Local\VDDInstaller"
 :: Delete any existing gpulist.txt
 del gpulist.txt 2>nul
 
-:: Run lfn.exe and create gpulist.txt
-lfn.exe > gpulist.txt
+:: Create a new gpulist.txt
+powershell -Command "Get-WmiObject Win32_VideoController | Where-Object { $_.AdapterRAM -gt 0 } | ForEach-Object { $_.Name } > '%USERPROFILE%\AppData\Local\VDDInstaller\gpulist.txt'"
