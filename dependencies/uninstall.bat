633bb80193013bb20adb815338b39b673551fadd
Bud <75859057+bud3699@users.noreply.github.com>
2024-12-19 08:47:57 +0000

    task kill whatever this command is on uninstall

--- dependencies/uninstall.bat
+++ dependencies/uninstall.bat
@@ -9,11 +9,13 @@ if defined vddPath (
     set "nefconwPath=%vddPath%\nefconw.exe"
     "%nefconwPath%" --remove-device-node --hardware-id ROOT\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
     taskkill /F /IM VDDSysTray.exe
+    taskkill /F /IM vdd_e-li_d-lo.cmd
     rmdir /s /q "%vddPath%"
     reg delete "HKLM\SOFTWARE\MikeTheTech\VirtualDisplayDriver" /f >nul 2>&1
 ) else (
     C:\VirtualDisplayDriver\nefconw.exe --remove-device-node --hardware-id ROOT\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
     taskkill /F /IM VDDSysTray.exe
+    taskkill /F /IM vdd_e-li_d-lo.cmd
     rmdir /s /q "C:\VirtualDisplayDriver"
 )
 rm dir /s /q "%USERPROFILE%\AppData\Local\VDDInstaller"
