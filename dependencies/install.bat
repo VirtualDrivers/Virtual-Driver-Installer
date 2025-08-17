ae1338274bc0582e57dc9cc6dee937646195c4d9
Mike Rodriguez <contact@mikethetech.com>
2025-08-17 12:29:50 -0700

    Updating Deps

--- dependencies/install.bat
+++ dependencies/install.bat
@@ -3,8 +3,8 @@ set "AppPath=%3"
 cd /d "%AppPath%"
 powershell -ExecutionPolicy Bypass -File "fixxml.ps1" %1 %2 %3
 :: installing VD-driver
-nefconw.exe --remove-device-node --hardware-id Root\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
-nefconw.exe --create-device-node --hardware-id Root\MttVDD --class-name Display --class-guid 4D36E968-E325-11CE-BFC1-08002BE10318
+"E:\GitHub\vdd-innosetupscript\input\Driver Files\VDD x86 x64\nefconw.exe" --remove-device-node --hardware-id Root\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
+"E:\GitHub\vdd-innosetupscript\input\Driver Files\VDD x86 x64\nefconw.exe" --create-device-node --hardware-id Root\MttVDD --class-name Display --class-guid 4D36E968-E325-11CE-BFC1-08002BE10318
 :: encapsulate that posible space in custum folder
-nefconw.exe --install-driver --inf-path "\"%AppPath%\MttVDD.inf\"" 
+"E:\GitHub\vdd-innosetupscript\input\Driver Files\VDD x86 x64\nefconw.exe" --install-driver --inf-path "\"E:\GitHub\vdd-innosetupscript\input\Driver Files\VDD x86 x64\MttVDD.inf\"" 
 exit
--- dependencies/uninstall.bat
+++ dependencies/uninstall.bat
@@ -6,26 +6,10 @@ for %%I in (%*) do (
 )
 
 if "%flag%"=="true" (
-    set "regPath=HKLM\SOFTWARE\MikeTheTech\VirtualDisplayDriver"
-    set "valueName=VDDPATH"
-
-    for /f "tokens=2* delims=    " %%A in ('reg query "%regPath%" /v "%valueName%" 2^>nul') do (
-        set "vddPath=%%B"
-    )
-    if defined vddPath (
-        set "nefconwPath=%vddPath%\nefconw.exe"
-        "%nefconwPath%" --remove-device-node --hardware-id ROOT\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
-        taskkill /F /IM VDDSysTray.exe
-        taskkill /F /IM vdd_e-li_d-lo.cmd
-        rmdir /s /q "%vddPath%"
-    ) else (
-        C:\VirtualDisplayDriver\nefconw.exe --remove-device-node --hardware-id ROOT\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
-        taskkill /F /IM VDDSysTray.exe
-        taskkill /F /IM vdd_e-li_d-lo.cmd
-        rmdir /s /q "C:\VirtualDisplayDriver"
-    )
-    reg delete "HKLM\SOFTWARE\MikeTheTech\VirtualDisplayDriver" /f >nul 2>&1
-    rm dir /s /q "%USERPROFILE%\AppData\Local\VDDInstaller"
+    "E:\GitHub\vdd-innosetupscript\input\Driver Files\VDD x86 x64\nefconw.exe" --remove-device-node --hardware-id ROOT\MttVDD --class-guid 4d36e968-e325-11ce-bfc1-08002be10318
+    taskkill /F /IM VDDSysTray.exe
+    taskkill /F /IM vdd_e-li_d-lo.cmd
+    rmdir /s /q "%USERPROFILE%\AppData\Local\VDDInstaller"
 ) else (
     start "" "unins000.exe"
 )
